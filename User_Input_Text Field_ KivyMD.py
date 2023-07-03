88281986be9f894836127e116a0f59f4a3418b41
Dr-Positive <116766276+Gokegold@users.noreply.github.com>
2023-07-03 03:42:31 +0100

    Update User_Input_Text Field_ KivyMD.py

--- User_Input_Text Field_ KivyMD.py
+++ User_Input_Text Field_ KivyMD.py
@@ -13,36 +13,39 @@ Location: Nigeria
 
 Date Created: JULY 2, 2023
 
-last modification:: [JULY 2, 2023]...
-
+last modification:: [JULY 2, 2023], [JULY 2, 2023]...
 """
-
 from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
+from kivymd.uix.button import MDRectangleFlatButton
 from kivymd.uix.textfield import MDTextField
 from kivy.lang import Builder
-
-username_helper = """
-MDTextField:
-    hint_text: "Enter username"
-    pos_hint: {'center_x': 0.5, 'center_y': 0.5}
-    size_hint_x:None
-    width:300
-"""
+from helpers import username_helper
 
 
 class DemoApp(MDApp):
 
     def build(self):
         screen = Screen()
+        self.theme_cls.primary_palette = "Blue"
         # username = MDTextField(text='Enter username',
         #                        pos_hint={'center_x': 0.5,
         #                                  'center_y': 0.5},
         #                        size_hint_x=None, width=300)
-
-        username = Builder.load_string(username_helper)
-        screen.add_widget(username)
+        # creating button instance
+        button = MDRectangleFlatButton(text='show',
+                                       pos_hint={'center_x': 0.5,
+                                                 'center_y': 0.4},
+                                       on_release=self.show_data)
+        self.username = Builder.load_string(username_helper)
+        screen.add_widget(self.username)
+        screen.add_widget(button)
         return screen
+
+        # to show the entered text.
+    def show_data(self, obj):
+        print(self.username.text)
+
 
 
 DemoApp().run()
